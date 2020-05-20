008c2109313ec169da6b800c62fbbe91deb3b7ab
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-05-20 14:09:56 +0100

    Completed TopBar

--- frontend/src/components/TopBar/TopBar.js
+++ frontend/src/components/TopBar/TopBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Mail from './../../img/mail.svg';
+import Phone from './../../img/phone.svg';
+import Facebook from './../../img/facebook.svg';
+import Twitter from './../../img/twitter.svg';
+import Instagram from './../../img/instagram.svg';
 import './TopBar.scss';
 function TopBar(props) {
     return (
@@ -9,10 +13,18 @@ function TopBar(props) {
                 <p className="topbar__one-email">Example@gmail.com</p>
             </div>
             <div className="topbar__two">
-                <p>Hello world</p>
+                <div className="topbar__two-phonecontainer">
+                    <img className="topbar__two-img" src={Phone} alt=""/>
+                    <p className="topbar__two-phone">+ 116 219 2222</p>
+                </div>
+                <img className="topbar__two-img" src={Facebook} alt="facebook"/>
+                <img className="topbar__two-img" src={Instagram} alt="Instagram"/>
+                <img className="topbar__two-img" src={Twitter} alt="twitter"/>
+
+
             </div>
             <div className="topbar__three">
-                <p>Hello world</p>
+                <p className="topbar__three-login">Login</p>
             </div>
         </div>
     );
